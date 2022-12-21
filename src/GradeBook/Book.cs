aa33028d03e578b3a1559a0d36185268fab01ebd
Ainemigisha <ainealvin96@gmail.com>
2022-12-21 16:27:05 +0300

    controlling the flow complete

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,37 +1,65 @@
-class Book
+public class Book
 {
     public Book(string name)
     {
         grades = new List<double>();
-        this.name = name;
+        Name = name;
     }
+
+    public void AddLetterGrade(char letter)
+    {
+
+    }
+
     public void AddGrade(double grade)
     {
-        grades.Add(grade);
+        if (grade <= 100 && grade >= 0)
+            grades.Add(grade);
+        else
+            throw new ArgumentException($"Invalid {nameof(grade)}");
     }
 
-    public void ShowStatistics()
+    public Statistics GetStatistics()
     {
+        var result = new Statistics();
+        result.Average = 0.0;
+        result.High = double.MinValue;
+        result.Low = double.MaxValue;
 
-        foreach (var number in grades)
+        foreach (var grade in grades)
         {
-            highGrade = Math.Max(number, highGrade);
-            lowGrade = Math.Min(number, lowGrade);
+            result.High = Math.Max(grade, result.High);
+            result.Low = Math.Min(grade, result.High);
 
-            result += number;
+            result.Average += grade;
         }
-        averageGrade = result / grades.Count;
+        result.Average /= grades.Count;
 
-        Console.WriteLine($"Average Grade is {result:N1}");
-        Console.WriteLine($"Highest Grade is {highGrade}");
-        Console.WriteLine($"Lowest value is {lowGrade}");
+        switch (result.Average)
+        {
+            case var d when d >= 90.0:
+                result.Letter = 'A';
+                break;
+            case var d when d >= 80.0:
+                result.Letter = 'B';
+                break;
+            case var d when d >= 70.0:
+                result.Letter = 'C';
+                break;
+            case var d when d >= 60.0:
+                result.Letter = 'D';
+                break;
+            default:
+                result.Letter = 'F';
+                break;
+        }
+
+        return result;
 
     }
 
     List<double> grades;
-    string name;
-    double highGrade = double.MinValue;
-    double lowGrade = double.MaxValue;
-    double result = 0.0, averageGrade;
+    public string Name;
+
 }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,17 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections.Generic;
 
- 
+
 
 var book = new Book("Picfare");
-book.AddGrade(9.7);
-book.AddGrade(12.4);
-book.AddGrade(20.1);
-book.AddGrade(39.1);
-book.AddGrade(9.08);
-
- book.ShowStatistics();
-  
 
 
+while (true)
+{
+    Console.WriteLine("Please enter a grade");
+    var input = Console.ReadLine();
+    if (input == null)
+        continue;
+    if (input == "q")
+        break;
+
+    try
+    {
+        book.AddGrade(double.Parse(input));
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+
+
+}
+
+var stats = book.GetStatistics();
+
+
+Console.WriteLine($"Average Grade is {stats.Average:N1}");
+Console.WriteLine($"Highest Grade is {stats.High}");
+Console.WriteLine($"Lowest Grade is {stats.Low}");
+Console.WriteLine($"Average Grade Letter is {stats.Letter}");
 
